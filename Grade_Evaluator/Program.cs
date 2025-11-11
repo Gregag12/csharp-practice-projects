d778dbe9240a4920011b101304b3fc5182203755
Garrett <I58993@verisk.com>
2025-11-10 17:34:49 -0700

    Added TODO comments and marked completed ones as DONE

--- Grade_Evaluator/Program.cs
+++ Grade_Evaluator/Program.cs
@@ -1,6 +1,9 @@
 ﻿//TODO: Prompt the user to enter a numeric grade (0-100)
 //TODO: Validate the input to ensure it is a number within the valid range
-//TODO: Create a method 'GetLetterGrade' to return letter grade
+//DONE: Create a method 'GetLetterGrade' to return letter grade
+//DONE: Create classes: Student, Subject, Grade
+//DONE: Create methods for GetStudentName, GetSubjectName, SelectSubjectFromList, GetFullName
+//DONE: Create subject enum
 //TODO: Display the letter grade
 //TODO: Aske the user if the want to evaluate another grade
 //TODO: Repeats until the user exists
